d3ca520dae9f1376be56084ae9527fff0da43434
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-02-28 22:46:33 +0800

    feat: add ultrasonic distance sensor reader

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# ...
-- ... will add more info after I refactored the codes to remove my very verbose logging
+# Description
+
+- This repo contains the code the will serve as the controller to our capstone project.
+
 # Reference
+
 - [Arduino Code](https://github.com/indrekluuk/LiveOV7670)
 - [Tutorial](https://circuitjournal.com/arduino-OV7670-to-pc)
---
+++ ultrasonic_sensor_reader.py
@@ -0,0 +1,32 @@
+import serial
+import time
+from datetime import timedelta
+
+port = "/dev/ttyUSB0" 
+baud_rate = 115_200
+
+ser = serial.Serial(port, baud_rate)
+
+start = time.time()
+
+data_list = []
+
+while True:
+    if len(data_list) == 10:
+        print(":: Process Completed")
+        print(data_list)
+        print(":: Average Distance:", sum(data_list)/len(data_list))
+        break
+    
+    if ser.in_waiting <= 0:
+        continue
+    if len(data_list) <= 0:
+        end = time.time()
+        print(f":: Got data after {timedelta(seconds=int(end)-(start))}")
+    
+    # TODO: parse the data
+    data = ser.read(ser.in_waiting)
+    distance = int(data.split(b':')[1].split(b'\r')[0].decode())
+    # WARN: this blindly accepts the reading, 
+    # WARN: additional calibration must be added to normalize the readings
+    data_list.append(distance)
